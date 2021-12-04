8fa10278715604fdbb64dd1bc5064ea80f61b36e
tejashriminde28 <95364821+tejashriminde28@users.noreply.github.com>
2021-12-04 18:26:15 +0530

    Add files via upload
    using,,,html,,php,cssjavascript

---
+++ birth_reg.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<style>
+	.error {color: #FF0000;}
+</style>
+<body>
+	<link rel="stylesheet" type="text/css" href="bir.reg.css">
+	<table border="3" cellspacing="10" cellpadding="10">
+		<td>
+			
+<h1>BIRTH_APPLICATION FORM</h1>
+
+
+  <label for="birthday">Birthday:</label>
+  <input type="date" id="birthday" name="birthday">
+  
+  <br><br>
+  <h2>Enter full name:</h2>
+  <label for="fname">First name:</label>
+  <input type="text" placeholder="enter first name" id="fname" name="fname" required=""><br><br>
+  <label for="fname">Middle  name:</label>
+  <input type="text" placeholder="enter middle name"id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" placeholder="enter last name"id="lname" name="lname"><br><br>
+  <form>
+  	<h2>Enter sex:</h2>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label>
+  <br><br>
+  
+  <label><h2>Address:</h2></label>
+ <textarea name="Address"></textarea>
+ <br><br>
+ <label><h2>Full name of father/husband:</h2></label>
+ <label for="fname">First name:</label>
+  <input type="text" placeholder="enter first name" id="fname" name="fname"><br><br>
+  <label for="fname">Middle  name:</label>
+  <input type="text" placeholder="enter middle name"id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" placeholder="enter last name"id="lname" name="lname"><br><br>
+
+  		<label><h2>Permanant address:</h2></label>
+  		<label for="fname">Flat/Room no.:</label>
+  <input type="text"placeholder="enter Flat/Room no." id="fname" name="fname"><br><br>
+  <label for="fname">Road/street no.:</label>
+  <input type="text" placeholder=" enter Road/street no."id="fname" name="fname"><br><br>
+  <label for="lname">city name:</label>
+  <input type="text" placeholder="enter city name "id="lname" name="lname"><br><br>
+  <label for="state">Select state:</label>
+  <select name="state" id="state">
+    <option value="Maharashtra">Maharashtra</option>
+    <option value="punjab">punjab</option>
+    <option value="Rajasthan">Rajasthan</option>
+    <option value="Andhrapradesh">Andhrapradesh</option>
+  </select>
+  <br><br>
+  <label for="lname">pincode:</label>
+  <input type="text" placeholder=" enter pincode"id="lname" name="lname"><br><br>
+ <label for="email">Enter your email:</label>
+  <input type="email" placeholder="Enter your email"id="email" name="email">
+  <br><br>
+  
+  <input type="submit" value="Submit">
+
+</form>
+ <br><br>
+  	</form>
+  </td>
+  </table>
+</form> 
+</form>
+</form>
+
+
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,43 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name']))
+ {
+
+ ?>
+
+   <!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<script src="https://kit.fontawesome.com/51c04ee047.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="bgimage">
+<header>
+	<div class="menu">
+	<div class="rightmenu">
+<ul>
+<li id="firstlist">HOME</li>
+<a href="signup.php"><li>USER</li></a>
+<a href="birth_reg.php"><li>ADMIN</li></a>
+</li>
+</ul>
+		</div>
+				</div>
+						</header>
+<main>
+	<section>
+		<div class="text">
+			</div>
+	
+</main>
+</section>
+<footer>
+<marquee width="100%" direction="scroll right to right" height="50%">
+    ONLINE  BIRTH REGISTRATION 
+</marquee>
+</footer>
+</div>
+</body>
+</html>
